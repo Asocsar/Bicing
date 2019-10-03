145434310e3bbb0c34a77bbcc38adbfdc2d38fd2
asocar <danielllisto@gmail.com>
2019-10-03 15:13:15 +0200

    hola

---
+++ src/src/Sucesores/sucesores.java
@@ -0,0 +1,13 @@
+package Sucesores;
+import aima.search.framework.SuccessorFunction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class sucesores implements SuccessorFunction{
+
+    public List getSuccessors(Object state){
+        ArrayList retVal = new ArrayList();
+        return retVal;
+    }
+}
