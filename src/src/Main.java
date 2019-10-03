58eb8deb38a1f36697aae48e7c387cffc440c5d0
asocar <danielllisto@gmail.com>
2019-10-03 21:27:21 +0200

    Basico

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,7 +1,47 @@
+import IA.Bicing.Estaciones;
 import IA.Bicing.Estacion;
 
 public class Main {
     public static void main(String[] args) {
-        Estacion e = new Estacion(2, 2);
+        int sumBic = 0;
+        int sumDem = 0;
+        int sumAvai = 0;
+        int sumNeed = 0;
+        Estaciones b = new Estaciones(25, 1250, 0, 433);
+        double[] stdDem = new double[b.size()];
+        double[] stdStay = new double[b.size()];
+        double[] stdCurr = new double[b.size()];
+        System.out.println("Sta Cur Dem Dif Exc ");
+
+        for(int i = 0; i < b.size(); ++i) {
+            int numStay = ((Estacion)b.get(i)).getNumBicicletasNoUsadas();
+            int numCurr = ((Estacion)b.get(i)).getNumBicicletasNext();
+            int numDem = ((Estacion)b.get(i)).getDemanda();
+            stdStay[i] = (double)numStay;
+            stdCurr[i] = (double)numCurr;
+            stdDem[i] = (double)numDem;
+            sumBic += numCurr;
+            sumDem += numDem;
+            int balance = numCurr - numDem;
+            int mover;
+            if (balance > 0) {
+                if (balance > numStay) {
+                    mover = numStay;
+                } else {
+                    mover = balance;
+                }
+
+                sumAvai += mover;
+            } else {
+                mover = 0;
+                sumNeed -= balance;
+            }
+
+            System.out.format("est %2d = %2d %2d\n", i, ((Estacion)b.get(i)).getCoordX(), ((Estacion)b.get(i)).getCoordY());
+            System.out.format("%3d %3d %3d %3d %3d\n", numStay, numCurr, numDem, balance, mover);
+        }
+
+        System.out.format("\nBicis= %3d Demanda= %3d Disponibles= %3d Necesitan= %3d\n\n", sumBic, sumDem, sumAvai, sumNeed);
     }
 }
+
