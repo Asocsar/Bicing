eded53f78a6813d39fbb9c5d6d5326dbacc08a63
asocar <danielllisto@gmail.com>
2019-10-03 15:31:48 +0200

    hola

--- src/src/Van/Van.java
+++ src/src/Van/Van.java
@@ -1,5 +1,26 @@
 package Van;
 
+import aima.util.Pair;
+
 public class Van {
+    private static int cap_max = 30;
+    private int CordX;
+    private int CordY;
+    private double long_t;
+    private int carga;
+    private int visited;
+
+    public Van (int x, int y) {
+        this.CordX = x*100;
+        this.CordY = y*100;
+        this.visited = 0;
+        this.carga = 0;
+        this.long_t = 0;
+
+    }
+
+    public int move (int x, int y) {
+
+    }
 
 }
