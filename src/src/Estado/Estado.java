16f98d29bdfd01f975471d7fc66929425f1bc23e
asocar <danielllisto@gmail.com>
2019-10-06 01:37:43 +0200

    Implementacion de Van y Concepto de Estado elemental

---
+++ src/src/Estado/Estado.java
@@ -0,0 +1,20 @@
+package Estado;
+
+import IA.Bicing.Estaciones;
+import Van.Van;
+
+import java.util.ArrayList;
+
+public class Estado {
+
+    private Estaciones Est;
+    private ArrayList<Van> V = new ArrayList<Van>();
+    private int cdesp;
+
+    public Estado (int num_est, int nbicis, int nfurgo, int demanda, int seed) {
+        this.Est = new Estaciones(num_est, nbicis, demanda, seed);
+
+
+
+    }
+}
--- src/src/Van/Van.java
+++ src/src/Van/Van.java
@@ -1,18 +1,19 @@
 package Van;
+import IA.Bicing.Estacion;
 
 public class Van {
     private static int cap_max = 30;
     private int CordX;
     private int CordY;
     private double long_t;
-    private int carga;
+    private int charge;
     private int visited;
 
     public Van (int x, int y) {
         this.CordX = x*100;
         this.CordY = y*100;
         this.visited = 0;
-        this.carga = 0;
+        this.charge = 0;
         this.long_t = 0;
 
     }
@@ -21,16 +22,27 @@ public class Van {
         double cost = Math.abs(this.CordX-x) + Math.abs(this.CordY-y);
         this.CordX = x;
         this.CordY = y;
-        return  cost;
+        return cost;
     }
 
-    public void pickUp(int number) {
-
+    public void pickUp(int number, Estacion e) {
+        if (this.charge + number > cap_max)  throw new IllegalStateException("The Van has exceed its maximum charge");
+        this.charge += number;
+        e.setNumBicicletasNoUsadas(e.getNumBicicletasNoUsadas()-number);
+        e.setNumBicicletasNext(e.getNumBicicletasNext()-number);
     }
 
-    public void leave(int number) {
-        visited += 1;
-        carga -= number;
+    public void leave(int number, Estacion e) {
+        if (this.charge < number)  throw new IllegalStateException("The Van doesn't have this number");
+        if (this.visited >= 2) throw new IllegalStateException("The Van has exceed the number of visited stations in 1 hour");
+        this.charge -= number;
+        this.visited += 1;
+        e.setNumBicicletasNext(e.getNumBicicletasNext()+number);
+        e.setNumBicicletasNoUsadas(e.getNumBicicletasNoUsadas()+number);
+    }
+
+    public void reset () {
+        this.visited = 0;
     }
 
 
