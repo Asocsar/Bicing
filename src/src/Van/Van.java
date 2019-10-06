39f6ea590b731c1a2b740f34bc288aa1f35c155d
asocar <danielllisto@gmail.com>
2019-10-06 18:14:45 +0200

    Final Van

--- src/src/Van/Van.java
+++ src/src/Van/Van.java
@@ -26,15 +26,12 @@ public class Van {
     }
 
     public void pickUp(int number, Estacion e) {
-        //if (this.charge + number > cap_max)  throw new IllegalStateException("The Van has exceed its maximum charge");
         this.charge += number;
         e.setNumBicicletasNoUsadas(e.getNumBicicletasNoUsadas()-number);
         e.setNumBicicletasNext(e.getNumBicicletasNext()-number);
     }
 
     public void leave(int number, Estacion e) {
-        //if (this.charge < number)  throw new IllegalStateException("The Van doesn't have this number");
-        //if (this.visited >= 2) throw new IllegalStateException("The Van has exceed the number of visited stations in 1 hour");
         this.charge -= number;
         this.visited += 1;
         e.setNumBicicletasNext(e.getNumBicicletasNext()+number);
@@ -44,6 +41,10 @@ public class Van {
     public void reset () {
         this.visited = 0;
     }
+
+    public int carga() { return this.charge; }
+
+    public int visitados () { return this.visited; }
 
 
 
