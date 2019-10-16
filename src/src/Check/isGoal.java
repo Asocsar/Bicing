0d4fbf14d2feffc18d642b9767d0d3b551ae4d55
asocar <danielllisto@gmail.com>
2019-10-16 16:12:04 +0200

    Clase Goal

---
+++ src/src/Check/isGoal.java
@@ -0,0 +1,12 @@
+package Check;
+
+import aima.search.framework.GoalTest;
+
+public class isGoal implements GoalTest {
+    public isGoal() {
+    }
+
+    public boolean isGoalState(Object state) {
+        return false;
+    }
+}
