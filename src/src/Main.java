8d3fcbaa0bd9ff305d853416a0f947a70f93276c
asocar <danielllisto@gmail.com>
2019-10-23 13:46:07 +0200

    R

--- src/src/Main.java
+++ src/src/Main.java
@@ -22,16 +22,18 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.Writer;
+
 public class Main {
     public Main() {
     }
 
     public static void main(String[] args) {
-        Estado Bicing = new Estado(30,1500,15,1,1001);
-        long StartTime = System.nanoTime();
+        Estado Bicing = new Estado(30,1500,15,0,1001);
         BicingHillClimbingSearch(Bicing);
-        long EndTime = System.nanoTime();
-        System.out.println("Execution in Miliseconds " + (EndTime-StartTime)/1000000);
         /*StartTime = System.nanoTime();
         BicingsimulatedAnnealingSearch(Bicing);
         EndTime = System.nanoTime();
@@ -42,19 +44,30 @@ public class Main {
         System.out.println("\nTSP HillClimbing  -->");
 
         try {
+            long StartTime = System.nanoTime();
             Problem problem = new Problem(TSPB, new sucesores(), new isGoal(), new Heuristic_Function());
             Search search = new HillClimbingSearch();
             SearchAgent agent = new SearchAgent(problem, search);
             System.out.println();
             Estado E = (Estado) search.getGoalState();
+            long EndTime = System.nanoTime();
+            System.out.println("Execution in Miliseconds " + (EndTime-StartTime)/1000000);
+            Writer output;
+            output = new BufferedWriter(new FileWriter("Estadisticas.txt", true));
+            Properties properties = agent.getInstrumentation();
+            String S = E.getganancia() + " " + (EndTime-StartTime/1000000) + " " + properties.getProperty((String)properties.keySet().iterator().next());
+            S = S + '\n';
+            output.append(S);
+            output.close();
 
+            /*
             for (int i = 0; i < E.getN_furgo(); ++i) {
                 System.out.println("Recorrido por furgoneta " + i + "  " + E.getIFurgo(i).getLong_t());
             }
-
+            */
             //printEstado(E);
-            printActions(agent.getActions());
-            printInstrumentation(agent.getInstrumentation());
+            //printActions(agent.getActions());
+            //printInstrumentation(agent.getInstrumentation());
         } catch (Exception var4) {
             var4.printStackTrace();
         }
