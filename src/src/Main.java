055f7d7aa444617b86b146dd8398f21f9d2cc85b
asocar <danielllisto@gmail.com>
2019-10-27 11:37:41 +0100

    boxplot beneficio, tiempo y nodos

--- src/src/Main.java
+++ src/src/Main.java
@@ -5,6 +5,8 @@
 
 import Estado.Estado;
 import Check.isGoal;
+import Heurisitc_Function_1.Heuristic_Function;
+//import Heuristic_Function_2.Heuristic_Function;
 import IA.Bicing.Estaciones;
 import IA.Bicing.Estacion;
 import Sucesores.sucesores;
@@ -22,35 +24,22 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 
-//import Heurisitc_Function_1.Heuristic_Function;
-import Heuristic_Function_2.Heuristic_Function;
-
 public class Main {
     public Main() {
     }
 
     public static void main(String[] args) throws IOException {
-        pruebaR(false,1);
+        pruebaR();
     }
 
-    public static void pruebaO (boolean h1, int d1) throws IOException {
+    public static void pruebaO () throws IOException {
         File file = new File("Sedds.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
-        String H = null;
-        if (h1) H = "1";
-        else H = "2";
-        int n_est = 25;
-        int nbicis = 1250;
-        int nfurgo = 5;
-        String D = null;
-        if (d1 == 1) D = "1";
-        else D = "0";
-        int cas = 0;
-        String S = "Ord";
         for (int i = 0; i < 100; ++i) {
             System.out.println(i);
             int s = Integer.parseInt(br.readLine());
-            BicingHillClimbingSearch(1, n_est, nbicis, nfurgo, d1, cas, S, H, D, s);
+            Estado Bicing = new Estado(25,1250,5,1,s,0);
+            BicingHillClimbingSearch(Bicing, 1);
         }
 
         //Estado Bicing = new Estado(25,1250,5,0,1234);
@@ -60,24 +49,14 @@ public class Main {
         System.out.println("Execution in Miliseconds " + (EndTime-StartTime)/1000000);*/
     }
 
-    public static void pruebaG (boolean h1, int d1) throws IOException {
+    public static void pruebaG () throws IOException {
         File file = new File("Sedds.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
-        String H = null;
-        if (h1) H = "1";
-        else H = "2";
-        int n_est = 25;
-        int nbicis = 1250;
-        int nfurgo = 5;
-        String D = null;
-        if (d1 == 1) D = "1";
-        else D = "0";
-        int cas = 1;
-        String S = "G";
         for (int i = 0; i < 100; ++i) {
             System.out.println(i);
             int s = Integer.parseInt(br.readLine());
-            BicingHillClimbingSearch(1, n_est, nbicis, nfurgo, d1, cas, S, H, D, s);
+            Estado Bicing = new Estado(25,1250,5,1,s,1);
+            BicingHillClimbingSearch(Bicing, 1);
         }
 
         //Estado Bicing = new Estado(25,1250,5,0,1234);
@@ -87,23 +66,13 @@ public class Main {
         System.out.println("Execution in Miliseconds " + (EndTime-StartTime)/1000000);*/
     }
 
-    public static void pruebaR (boolean h1, int d1) throws IOException {
+    public static void pruebaR () throws IOException {
         File file = new File("Sedds.txt");
-        String H = null;
-        if (h1) H = "1";
-        else H = "2";
-        int n_est = 25;
-        int nbicis = 1250;
-        int nfurgo = 5;
-        String D = null;
-        if (d1 == 1) D = "1";
-        else D = "0";
-        int cas = 2;
-        String S = "Rnd";
         BufferedReader br = new BufferedReader(new FileReader(file));
         for (int i = 0; i < 100; ++i) {
             int s = Integer.parseInt(br.readLine());
-            BicingHillClimbingSearch(20, n_est, nbicis, nfurgo, d1, cas, S, H, D, s);
+            Estado Bicing = new Estado(25, 1250, 5, 1, s, 2);
+            BicingHillClimbingSearch(Bicing, 20);
         }
         /*
         System.out.println("Execution in Miliseconds " + (EndTime-StartTime)/1000000);
@@ -113,7 +82,7 @@ public class Main {
         System.out.println("Execution in Miliseconds " + (EndTime-StartTime)/1000000);*/
     }
 
-    private static void BicingHillClimbingSearch(int num, int n_est, int nbicis, int nfurgo, int d1, int cas, String Cas, String H, String D, int s) {
+    private static void BicingHillClimbingSearch(Estado Bicing, int num) {
         //System.out.println("\nTSP HillClimbing  -->");
 
 
@@ -122,9 +91,9 @@ public class Main {
             double MedN = 0;
             double MedB = 0;
             for (int i = 0; i < num; ++i) {
-                Estado Bicing = new Estado(n_est,nbicis,nfurgo,d1,s,cas);
+                Estado TSPB = Bicing.clonar();
                 long StartTime = System.nanoTime();
-                Problem problem = new Problem(Bicing, new sucesores(), new isGoal(), new Heuristic_Function());
+                Problem problem = new Problem(TSPB, new sucesores(), new isGoal(), new Heuristic_Function());
                 Search search = new HillClimbingSearch();
                 SearchAgent agent = new SearchAgent(problem, search);
                 Properties properties = agent.getInstrumentation();
@@ -140,7 +109,7 @@ public class Main {
             MedT /= num;
             MedB = (Math.round(MedB*100.0)/100.0);
             Writer output;
-            output = new BufferedWriter(new FileWriter("Estadisticas_" + Cas + "_D" + D + "_H" + H + ".txt", true));
+            output = new BufferedWriter(new FileWriter("Estadisticas_Rnd_D1_H1_1A.txt", true));
             String sep = ",";
             String S = MedB + sep + MedT + sep + MedN;
             S = S + '\n';
